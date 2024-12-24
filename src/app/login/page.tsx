16ad5eef0d759fe577f7438f0e3a9e523c7f2bf3
vishalthapa07 <vishalqubits@gmail.com>
2024-12-24 15:46:48 +0545

    project setup

---
+++ src/app/login/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Login() {
+  return <div>page</div>;
+}
+
+export default Login;
---
+++ src/helpers/mailer.ts
@@ -0,0 +1,15 @@
+import nodemailer from "nodemailer";
+
+export const sendEmail = async ({ email, emailType, userId }) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      host: "smtp.ethereal.email",
+      port: 587,
+      secure: false, // true for port 465, false for other ports
+      auth: {
+        user: "maddison53@ethereal.email",
+        pass: "jn7jnAPss4f63QBp6D",
+      },
+    });
+  } catch (error) {}
+};
